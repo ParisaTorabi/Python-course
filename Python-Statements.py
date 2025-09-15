7e258e603f970ceade36c940569c4a3eee6ba6dd
Parisa Torabi <parisatorabi7@gmail.com>
2025-09-15 13:57:35 +0200

    Finished the if-statement assignment.

--- Python-Statements.py
+++ Python-Statements.py
@@ -7,13 +7,15 @@ If the purchase amount is between 20,000 and 50,000 Tomans, a 10% discount is ap
 If the purchase amount is less than 20,000 Tomans, no discount is applied.
 
 Write a program that receives the purchase amount from the user and displays the final amount.
+
+Test: an item valued 55_000 should be sold for 44_000.
 """
 
 if __name__ == "__main__":
     purchase_amount = int(input("Enter your purchase amount:"))
     if purchase_amount > 50_000:
-        print(0.8 * purchase_amount)
+        print(f"The bill is for {0.8 * purchase_amount} Tomans.")
     elif 20_000 <= purchase_amount < 50_000:
-        print(0.9 * purchase_amount)
+        print(f"You should pay {0.9 * purchase_amount} Tomans.")
     else:
-        print(purchase_amount)
+        print(f"The amount to pay is the same as the price, {purchase_amount} Tomans.")
