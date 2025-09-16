e929a6f492a51ead801cb59740974295733e4c5e
Parisa Torabi <parisatorabi7@gmail.com>
2025-09-16 15:54:10 +0200

    complete loop exercise- test ok

--- Python-loops.py
+++ Python-loops.py
@@ -52,3 +52,38 @@ for person, data in People.items():
 
 for data in People.values():
     print(data)
+# %%
+# Exercise
+"""A wizard has decided to increase the power of numbers by using a special spell! He chooses a number n and then does the following for n steps:
+
+If the number is odd, double its value and subtract 1 unit.
+
+If the number is even, halve its value.
+
+The wizard performs this operation exactly n times.
+📝 Input:
+
+An integer n that specifies the number of steps (1 ≤ n ≤ 20).
+An integer x that is the initial value (1 ≤ x ≤ 1000).
+
+📤 Output: Finally, display the final value of x.
+Input 1:
+
+5
+10
+
+Output 1:
+
+65"""
+
+if __name__ == "__main__":
+    n = int(input("insert number of steps, n:"))
+    x = int(input("insert initial value, x:"))
+
+    for _ in range(n):
+        if x % 2 == 0:
+            x = x / 2
+        else:
+            x = (2 * x) - 1
+
+    print(f"Final value is {x}")
