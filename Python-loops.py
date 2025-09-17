1831b176c83696088a0dc432d53fa1e40e5a29f3
Parisa Torabi <parisatorabi7@gmail.com>
2025-09-17 10:51:17 +0200

    add while loop exercise

--- Python-loops.py
+++ Python-loops.py
@@ -87,3 +87,69 @@ if __name__ == "__main__":
             x = (2 * x) - 1
 
     print(f"Final value is {x}")
+# %% While loop
+a = 0
+
+while a <= 5:
+    print(f"a is {a}")
+    a += 1
+
+# %% While exercise
+
+"""We have a machine called the "magic machine" that takes an initial number n and wants to get to 1. The machine can do two things:
+
+If the current number is divisible by 2, it halve it.
+If the current number is not divisible by 2, it multiplies the number by 3 and adds 1 to it.
+
+Your task is to write a program that takes the initial number n as input and show how the machine gets to 1.
+
+Input:
+
+An integer n (2 ≤ n ≤ 1000)
+
+Output:
+
+At each step, print the current value of n until the number reaches 1.
+
+Sample Input:
+
+50
+
+Sample Output:
+
+50
+25
+76
+38
+19
+58
+29
+88
+44
+22
+11
+34
+17
+52
+26
+13
+40
+20
+10
+5
+16
+8
+4
+2
+1
+"""
+
+n = int(input("input the initial number, n:"))
+
+while n != 1:
+    print(n)
+    if n % 2 == 0:
+        n = n / 2
+    else:
+        n = (n * 3) + 1
+print(n)
