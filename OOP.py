87c914f7b555711cd13601eda89d003c578aa3d4
Parisa Torabi <parisatorabi7@gmail.com>
2025-10-06 13:24:44 +0200

    Add examples on inheretence and polymorphism

--- OOP.py
+++ OOP.py
@@ -1,3 +1,4 @@
+# %%
 import random
 
 
@@ -36,3 +37,61 @@ class Circle:
 
 C1 = Circle(4)
 print(C1.compute_area())
+
+
+# %% Inheritance
+
+
+class Educational(Book):
+    def __init__(self, name, pages, major, seniority):
+        super().__init__(name, pages)
+        self.major = major
+        self.seniority = seniority
+
+
+eb1 = Educational("Biology tests", 234, "natural sciences", "Sophomore")
+
+print(eb1.name)
+eb1.open_a_random_page()
+print(eb1.seniority)
+
+# %% overriding...
+
+
+class fiction(Book):
+    genre = "Fiction"
+
+    def open_a_random_page(self):
+        print(f"Opened the last page {self.pages}, because fiction works like that!")
+
+
+fb = fiction("Harry Potter and the Goblet of Fire", 636)
+fb.open_a_random_page()
+print(fb.genre)
+
+# %% On polymorphism
+
+
+class Runner:
+    def __init__(self, name: str):
+        self.name = name
+
+    def act(self):
+        print(f"{self.name} is running.")
+
+
+class Cyclist:
+    def __init__(self, name: str):
+        self.name = name
+
+    def act(self):
+        print(f"{self.name} is biking.")
+
+
+Parisa = Cyclist("Parisa")
+Erfan = Runner("Erfan")
+
+for person in [Parisa, Erfan]:
+    person.act()  # Interesting!!
+
+# %% abstract methods
